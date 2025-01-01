e7a5cc8459ef791571d86d97666bd5abefb71e16
Jerry <x.jerry.wang@gmail.com>
2025-01-01 23:10:24 +0800

    feat: implement basic function

---
+++ README.md
@@ -0,0 +1,19 @@
+# [Rembg] Background Removal Node for [ComfyUI]
+
+Many thanks to the author of **[rembg-comfyui-node](https://github.com/Jcd1230/rembg-comfyui-node)** for his very nice work, this is a very useful tool!
+
+## How to use
+
+1. Install this extension
+
+```
+git clone https://github.com/0x-jerry/comfyui-rembg.git
+```
+
+2. Download models from the [Rembg models][rembg-models] and put them in the `models/rembg` directory.
+
+3. To use it, just look for the `Image Remove Background (rembg)` node and select the `model` you want to use!
+
+[rembg]: https://github.com/danielgatis/rembg
+[rembg-models]: https://github.com/danielgatis/rembg?tab=readme-ov-file#models
+[comfyui]: https://github.com/comfyanonymous/ComfyUI
---
+++ __init__.py
@@ -0,0 +1,51 @@
+import torch
+import torchvision.transforms.v2 as T
+import folder_paths
+from rembg import new_session, remove
+
+class RemoveImageBackgroundNode:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        model_names = [x for x in folder_paths.get_filename_list("rembg")]
+
+        return {
+            "required": {
+                "model_name": (model_names, ),
+                "image": ("IMAGE",),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE", "MASK",)
+    FUNCTION = "remove_background"
+    CATEGORY = "Rembg"
+    TITLE = "Image Remove Background"
+
+
+    def remove_background(self, image, model_name):
+        model_path = folder_paths.get_full_path("rembg", model_name)
+        
+        session = new_session(model_path, providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+
+        image = image.permute([0, 3, 1, 2])
+        output = []
+        for img in image:
+            img = T.ToPILImage()(img)
+            img = remove(img, session=session)
+            output.append(T.ToTensor()(img))
+
+        output = torch.stack(output, dim=0)
+        output = output.permute([0, 2, 3, 1])
+        mask = output[:, :, :, 3] if output.shape[3] == 4 else torch.ones_like(output[:, :, :, 0])
+
+        return(output, mask,)
+
+
+# A dictionary that contains all nodes you want to export with their names
+NODE_CLASS_MAPPINGS = {
+    "Remove Image Background": RemoveImageBackgroundNode
+}
+
+
